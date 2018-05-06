608d8ac4a8a1d1d04661b947496a20990cb1f8b7
SophieHYe <He_Ye_90s@hotmail.com>
2018-05-06 18:57:49 +0200

    added programs

---
+++ src/java_programs_missing_line/DETECT_CYCLE3.java
@@ -0,0 +1,30 @@
+package java_programs_missing_line;
+import java.util.*;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java_programs_correct.Node;
+
+/**
+ *
+ * @author derricklin
+ */
+public class DETECT_CYCLE3 {
+    public static boolean detect_cycle(Node node) {
+        Node hare = node;
+        Node tortoise = node;
+
+        while (true) {
+            if (null==hare ||hare.getSuccessor() == null)
+                return false;
+
+            tortoise = tortoise.getSuccessor();
+            //hare = hare.getSuccessor().getSuccessor();
+
+            if (hare == tortoise)
+                return true;
+        }
+    }
+}
